58b9cfe63e7aaac1e19c9482fc57b2c7a3588a63
Daniel Hampel <danielhampel.danham@gmail.com>
2023-10-12 19:43:59 +0200

    implementation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,64 +1,40 @@
-﻿using System.Threading.Tasks;
-
-namespace AR_Test;
+﻿namespace AR_Test;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
-
-    protected override async void OnAppearing()
+    private async void WebViewPermissionRequest(object sender, EventArgs e)
     {
-        base.OnAppearing();
 
-        if (!await CheckPermission())
-        {
-			Console.WriteLine("PERMISSION NOT GRANTED!!!");
-            //Application.Current.Quit();
-        } else
-		{
-            WebView myWebView = new WebView
-            {
-                Source = new UrlWebViewSource
-                {
-                    Url = "https://ar-js-org.github.io/.github/profile/aframe/examples/image-tracking/nft/" // Die URL Ihrer Webseite
-                }
-            };
-
-            Content = myWebView;
-
-            myWebView.Eval("navigator.mediaDevices.getUserMedia({ video: true })");
-
-            Console.WriteLine("PERMISSION GRANTED!!!");
-		}
-    }
-
-	private async Task<bool> CheckPermission()
-	{
+        String url = "https://stemkoski.github.io/AR-Examples/hello-cube.html";
+        // Überprüfen, ob die Kamera-Berechtigung erteilt wurde
         PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.Camera>();
 
         if (status != PermissionStatus.Granted)
         {
+            // Berechtigung für die Kamera anfordern
             status = await Permissions.RequestAsync<Permissions.Camera>();
-        }
 
-        return status == PermissionStatus.Granted;
+            if (status == PermissionStatus.Granted)
+            {
+                // Kamera-Berechtigung wurde gewährt
+                webview.Source = url; // Setzen Sie hier die URL der Webseite ein
+            }
+            else
+            {
+                // Kamera-Berechtigung wurde nicht gewährt, zeigen Sie eine Fehlermeldung an oder nehmen Sie andere Maßnahmen
+            }
+        }
+        else
+        {
+            // Kamera-Berechtigung wurde bereits erteilt
+            webview.Source = url; // Setzen Sie hier die URL der Webseite ein
+        }
     }
 }
 
--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,10 +1,24 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using MauiAppWebViewHandlers.Platforms.Android;
 
 namespace AR_Test;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        CustomizeWebViewHandler();
+    }
+
+    private static void CustomizeWebViewHandler()
+    {
+        Microsoft.Maui.Handlers.WebViewHandler.Mapper.ModifyMapping(
+            nameof(Android.Webkit.WebView.WebChromeClient),
+            (handler, view, args) => handler.PlatformView.SetWebChromeClient(new MyWebChromeClient(handler)));
+    }
 }
